e5bb8d0bf48c0fff6646007b075f7ade7ecd0b6f
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-12-12 23:18:34 +0000

    Implements weight normalised Conv2d layer

--- model.py
+++ model.py
@@ -1,8 +1,35 @@
 import torch
-import torch.nn
+import torch.nn as nn
 import torch.nn.functional as F
 
 from functools import lru_cache
+
+class WNConv2d(nn.Conv2d):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        torch.nn.utils.weight_norm(self)
+        self.forward = super().forward
+
+class GatedResBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return x
+
+class CausalAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return x
+
+class PixelBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return x
 
 class PixelSnail(nn.Module):
     def __init__(self):
@@ -12,4 +39,5 @@ class PixelSnail(nn.Module):
         return x
 
 if __name__ == "__main__":
-    print("Aloha, World!")
+    wn_conv = WNConv2d(1, 8, 3)
+    print(wn_conv)
