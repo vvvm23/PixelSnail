54bed843fc8528a59fdcd55b6a17992ee73c3c13
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-12-13 14:16:59 +0000

    Fixes bug in sampling

---
+++ sample.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn.functional as F
+import torchvision
+
+import sys
+import time
+from tqdm import tqdm
+from model import PixelSnail
+
+TRY_CUDA = True
+IMAGE_DIM = [28,28]
+NB_SAMPLES = 4
+
+device = torch.device('cuda' if TRY_CUDA and torch.cuda.is_available() else 'cpu')
+print(f"> Using device {device}")
+
+try:
+    print(f"> Loading PixelSnail from file {sys.argv[1]}")
+    model = PixelSnail(IMAGE_DIM, 256, 32, 5, 3, 2, 16, nb_out_res_block=2).to(device)
+    model.load_state_dict(torch.load(sys.argv[1]))
+    model.eval()
+    print("> Loaded PixelSnail succesfully!")
+except:
+    print("! Failed to load state dict!")
+    print("! Make sure model is of correct size and path is correct!")
+    exit()
+
+with torch.no_grad():
+    sample = torch.zeros(NB_SAMPLES, *IMAGE_DIM, dtype=torch.int64).to(device)
+    pb = tqdm(total=IMAGE_DIM[0]*IMAGE_DIM[1])
+
+    cache = {}
+    for i in range(IMAGE_DIM[0]):
+        for j in range(IMAGE_DIM[1]):
+            pred, cache = model(sample, cache=cache)
+            pred = pred.to(device)
+            pred = F.softmax(pred[:, :, i, j], dim=1)
+            sample[:, i, j] = torch.multinomial(pred, 1).float().squeeze()
+            pb.update(1)
+
+    save_id = int(time.time())
+    torchvision.utils.save_image(sample.unsqueeze(1) / 255., f"samples/zero-{save_id}.png", nrow=10)
